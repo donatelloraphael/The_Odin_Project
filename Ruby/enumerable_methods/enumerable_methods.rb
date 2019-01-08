81423fdebf49338e76a470971db2274cc86d7733
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2019-01-08 14:58:49 +0530

    finish advanced building blocks exercises

--- Ruby/enumerable_methods/enumerable_methods.rb
+++ Ruby/enumerable_methods/enumerable_methods.rb
@@ -110,8 +110,51 @@ module Enumerable
 			self.my_each do |x|
 				count += 1 if yield(x)
 			end
+			count
 		end
+	end
 
+	def my_map(&proc)
+		return self.to_enum unless block_given?
+		new_array = []
+		if self.class == Hash
+			self.each do |k, v|
+				new_array << proc.call(k, v)
+			end
+			new_array
+		else
+			self.my_each do |x|
+				new_array << proc.call(x)
+			end
+			new_array
+		end
+	end
 
+	def my_inject(*args)
+		total = 0
+    if args[0].class == Symbol
+      sym = args[0]
+      total = self[0]
+      self[1..-1].my_each do |i|
+        total = total.send(sym, i)
+      end
+    elsif args.length != 0
+      total = args[0]
+      self.my_each do |i|
+        total = yield(total,i)
+      end
+    else
+      total = self[0]
+      self[1..-1].my_each do |i|
+        total = yield(total,i)
+      end
+    end
+	end
+
+	def multiply_elements
+  	self.my_inject {|total, n| total * n}
+	end
+
+	puts [1,2,3,4,5].multiply_elements
 
 end
